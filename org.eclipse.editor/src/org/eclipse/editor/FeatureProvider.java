89c0fd7e37e86cbe73e356bdace7a57b7d837522
Marko Alas <markoa@gmail.com>
2011-03-16 09:40:55 +0200

    no metadata

--- org.eclipse.editor/src/org/eclipse/editor/FeatureProvider.java
+++ org.eclipse.editor/src/org/eclipse/editor/FeatureProvider.java
@@ -1,10 +1,32 @@
 package org.eclipse.editor;
 
+import org.eclipse.editor.features.AddEClassFeature;
+import org.eclipse.editor.features.CreateFeature;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.graphiti.dt.IDiagramTypeProvider;
+import org.eclipse.graphiti.features.IAddFeature;
+import org.eclipse.graphiti.features.ICreateFeature;
+import org.eclipse.graphiti.features.context.IAddContext;
 import org.eclipse.graphiti.ui.features.DefaultFeatureProvider;
 
 public class FeatureProvider extends DefaultFeatureProvider {
 	public FeatureProvider(IDiagramTypeProvider dtp) {
 		super(dtp);
 	}
+	
+	@Override
+    public IAddFeature getAddFeature(IAddContext context) {
+        // is object for add request a EClass?
+        if (context.getNewObject() instanceof EClass) {
+            return new AddEClassFeature(this);
+
+        }
+
+        return super.getAddFeature(context);
+    }
+	
+	@Override
+    public ICreateFeature[] getCreateFeatures() {
+        return new ICreateFeature[] { new CreateFeature(this) };
+    }
 }
